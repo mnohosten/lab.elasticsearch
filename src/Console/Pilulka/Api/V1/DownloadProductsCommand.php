68966c7231b2fa4d8cb556ceb06e78461f1de4a8
Martin Křižan <mnohosten@gmail.com>
2019-04-26 16:06:40 +0200

    API iterator

--- src/Console/Pilulka/Api/V1/DownloadProductsCommand.php
+++ src/Console/Pilulka/Api/V1/DownloadProductsCommand.php
@@ -74,7 +74,6 @@ class DownloadProductsCommand extends Command
                         }
                     }
                 }
-                break;
             }
             if($hasEmptyResponse) break;
         }
